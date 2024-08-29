987fe620200ae190c1d6ff4215d553f63c46918b
Pedro <pedrolucasazevedodias@gmail.com>
2024-08-29 09:46:04 -0300

    update BaseModelInterface

--- database/interfaces/repositories/base.py
+++ database/interfaces/repositories/base.py
@@ -1,27 +1,47 @@
+from ..models import BaseModelInterface
+from typing import List, Optional, Union
 from abc import ABC, abstractmethod
 
 class BaseRepositoryInterface(ABC):
     @abstractmethod
-    def create(self, entity):
-        """Insere uma nova entidade no repositório."""
+    def create(self, *models: BaseModelInterface) -> Union[List[str], Optional[str]]:
+        """
+        Insere os dados de um ou mais Models no repositório e retorna os IDs.
+
+        Retorna None caso não consiga inserir os dados.
+        """
         pass
 
     @abstractmethod
-    def read(self, entity_id):
-        """Lê uma entidade específica com base no ID."""
+    def read(self, query: dict = None) -> List[BaseModelInterface] :
+        """
+        Retorna uma lista de Models com base na query.
+        """
         pass
 
     @abstractmethod
-    def update(self, entity):
-        """Atualiza uma entidade existente no repositório."""
+    def update(self, query: dict = None, update_data: dict = None) -> List[str]:
+        """
+        Atualiza os dados no repositório de acordo com a query.
+
+        Retorna retorna uma lista com os IDs dos dados alterados.
+
+        Se nenhuma query ou update_data for fornecida, não realiza nenhuma atualização.
+        """
         pass
 
     @abstractmethod
-    def delete(self, entity_id):
-        """Remove uma entidade do repositório com base no ID."""
+    def delete(self, model_id: str) -> Optional[BaseModelInterface]:
+        """
+        Remove uma entidade do repositório com base no ID.
+
+        Retorna os dados removidos do repositório ou None casa não seja possível.
+        """
         pass
 
     @abstractmethod
-    def list(self):
-        """Lista todas as entidades no repositório."""
+    def list(self) -> List[BaseModelInterface]:
+        """
+        Lista todas as entidades no repositório.
+        """
         pass
